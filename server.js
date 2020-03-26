23dd5d2f3000d8e879b521ad02e067281a15a65a
Dan <dmcg926@gmail.com>
2020-03-26 14:57:27 -0500

    created bootcamp model and updated get, post, put, and delete methods in controllers/bootcamps

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -29,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightMagenta));
 
-// Handle unhandled promis rejections
+// Handle unhandled promise rejections
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red);
