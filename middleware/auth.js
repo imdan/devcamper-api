33abf88bbb2d6c2a390ed1f80a9aac0f4cd0338b
Dan <dmcg926@gmail.com>
2020-04-13 19:23:23 -0500

    add logout auth method, and added packages to prevent nosql injection, sanitize data, provide xss protection and security headers, rate limiting, HPP and CORS

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,10 +8,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        // Set token from bearer token in header
         token = req.headers.authorization.split(' ')[1];
-    }
-
+    } 
     // else if (req.cookies.token) {
+    //     // set token from cookie
     //     token = req.cookies.token;
     // }
 
